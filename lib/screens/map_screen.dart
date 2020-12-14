93343be5869dbaaadabb03764e2dcaa6c6ea08d3
delinuxist <delinuxist@gmail.com>
2020-12-14 13:40:23 +0000

    map screen file

---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import '../models/places.dart';
+
+class MapScreen extends StatefulWidget {
+  static const nameRoute = '/mapScreen';
+
+  final PlaceLocation initialLocation;
+  final bool isSelecting;
+
+  MapScreen(
+      {this.initialLocation =
+          const PlaceLocation(latitude: 5.574090, longitude: -0.286460),
+      this.isSelecting = false});
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  LatLng _pickedLocation;
+
+  void _selectLocation(LatLng position) {
+    setState(() {
+      _pickedLocation = position;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Map'),
+        actions: [
+          if (widget.isSelecting)
+            IconButton(
+              icon: Icon(Icons.check),
+              onPressed: _pickedLocation == null
+                  ? null
+                  : () {
+                      Navigator.of(context).pop(_pickedLocation);
+                    },
+            ),
+        ],
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(widget.initialLocation.latitude,
+              widget.initialLocation.longitude),
+          zoom: 16,
+        ),
+        onTap: widget.isSelecting ? _selectLocation : null,
+        markers: _pickedLocation == null
+            ? null
+            : {
+                Marker(markerId: MarkerId('m1'), position: _pickedLocation),
+              },
+      ),
+    );
+  }
+}
